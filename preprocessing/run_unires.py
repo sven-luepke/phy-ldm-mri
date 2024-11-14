875fc6967ca514788f7a2b7d2c11a9013c3b2266
Sven Lüpke <luepke@in.tum.de>
2024-11-14 17:50:25 +0100

    Add unires to preprocessing

--- preprocessing/run_unires.py
+++ preprocessing/run_unires.py
@@ -1,26 +1,16 @@
 import argparse
 import os
 import subprocess
-import math
-
-
-def split_list_evenly(lst, n):
-    sublist_size =math. ceil(len(lst) / n)
-    return [lst[i:i + sublist_size] for i in range(0, len(lst), sublist_size)]
 
 
 def main():
     parser = argparse.ArgumentParser("Run UniRes on the dataset")
     parser.add_argument("--data", type=str)
-    parser.add_argument("--split_count", type=int, default=1)
-    parser.add_argument("--split_index", type=int, default=0)
     args = parser.parse_args()
 
     dataset_path = args.data
 
     session_dirs = sorted(os.listdir(dataset_path))
-
-    session_dirs = split_list_evenly(session_dirs, args.split_count)[args.split_index]
 
     for session in session_dirs:
         session_path = os.path.join(dataset_path, session)
